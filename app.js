803af37b11120321af75118c39a57b7566362ba5
Dani Vicario <danigv.email@gmail.com>
2018-11-19 01:33:44 +0100

    added partials

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require("express");
-const authRoutes = express.Router();
+
+const app = express()
 const session = require("express-session");
 const bcrypt = require("bcrypt");
+// Bcrypt to encrypt passwords
+const bcryptSalt = 10;
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const ensureLogin = require("connect-ensure-login");
 const flash = require("connect-flash");
+const mongoose = require("mongoose")
+const bodyParser = require('body-parser');
+const path = require('path')
+const hbs = require('hbs')
+
+Swag = require('swag');
+
+Swag.registerHelpers(hbs);
+
+const User = require("./models/user");
+
+mongoose.Promise = Promise;
+mongoose
+	.connect('mongodb://localhost/basic-auth')
+	.then(() => {
+		console.log('Connected to Mongo!')
+	}).catch(err => {
+		console.error('Error connecting to mongo', err)
+	});
 
 app.use(session({
 	secret: "our-passport-local-strategy-app",
@@ -15,18 +37,14 @@ app.use(session({
 
 app.use(flash());
 
-passport.serializeUser((user, cb) => {
-	cb(null, user._id);
-});
+app.use(express.static(path.join(__dirname, '/public')));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
-passport.deserializeUser((id, cb) => {
-	User.findById(id, (err, user) => {
-		if (err) {
-			return cb(err);
-		}
-		cb(null, user);
-	});
-});
+app.use(bodyParser.urlencoded({
+	extended: true,
+}));
 
 passport.use(new LocalStrategy({
 	passReqToCallback: true
@@ -55,24 +73,40 @@ passport.use(new LocalStrategy({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// User model
-const User = require("../models/user");
-
-// Bcrypt to encrypt passwords
-const bcrypt = require("bcrypt");
-const bcryptSalt = 10;
-
-authRoutes.get("/signup", (req, res, next) => {
-	res.render("auth/signup");
+passport.serializeUser((user, cb) => {
+	cb(null, user._id);
 });
 
-authRoutes.post("/signup", (req, res, next) => {
+passport.deserializeUser((id, cb) => {
+	User.findById(id, (err, user) => {
+		if (err) {
+			return cb(err);
+		}
+		cb(null, user);
+	});
+});
+
+app.get('/', ensureLogin.ensureLoggedIn(), (req, res) => {
+	res.render('base', {
+		user: req.user,
+		section: 'index'
+	})
+})
+
+app.get("/signup", (req, res, next) => {
+	res.render("base", {
+		section: 'signup'
+	});
+});
+
+app.post("/signup", (req, res, next) => {
 	const username = req.body.username;
 	const password = req.body.password;
 
 	if (username === "" || password === "") {
 		res.render("auth/signup", {
-			message: "Indicate username and password"
+			message: "Indicate username and password",
+			"section": "private"
 		});
 		return;
 	}
@@ -82,8 +116,9 @@ authRoutes.post("/signup", (req, res, next) => {
 		})
 		.then(user => {
 			if (user !== null) {
-				res.render("auth/signup", {
-					message: "The username already exists"
+				res.render("base", {
+					message: "The username already exists",
+					"section": "signup"
 				});
 				return;
 			}
@@ -98,8 +133,9 @@ authRoutes.post("/signup", (req, res, next) => {
 
 			newUser.save((err) => {
 				if (err) {
-					res.render("auth/signup", {
-						message: "Something went wrong"
+					res.render("base", {
+						message: "Something went wrong",
+						"section": "signup"
 					});
 				} else {
 					res.redirect("/");
@@ -111,26 +147,30 @@ authRoutes.post("/signup", (req, res, next) => {
 		})
 });
 
-authRoutes.get("/login", (req, res, next) => {
-	res.render("auth/login", {
-		"message": req.flash("error")
+app.get("/login", (req, res, next) => {
+	res.render("base", {
+		"message": req.flash("error"),
+		"section": "login"
 	});
 });
 
-authRoutes.post("/login", passport.authenticate("local", {
-	successRedirect: "/",
+app.post("/login", passport.authenticate("local", {
+	successReturnToOrRedirect: "/",
 	failureRedirect: "/login",
 	failureFlash: true,
 	passReqToCallback: true
 }));
 
-authRoutes.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-	res.render("private", {
-		user: req.user
+app.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+	res.render("base", {
+		user: req.user,
+		"section": "private"
 	});
 });
 
-authRoutes.get("/logout", (req, res) => {
+app.get("/logout", (req, res) => {
 	req.logout();
 	res.redirect("/login");
 });
+
+app.listen(3000)
