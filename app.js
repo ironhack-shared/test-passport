dc1d71c4b416c29adfe99a4313664222f4d8f3f5
daniel vicario <danigv.email@gmail.com>
2018-11-19 13:54:06 +0100

    added comments

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
+//web server
 const express = require("express");
-
+//express instantiation
 const app = express()
+//session management. we need this so we can hold the user id
 const session = require("express-session");
+//cipher algorithm with on-purpose delay
 const bcrypt = require("bcrypt");
-// Bcrypt to encrypt passwords
+// Bcrypt to encrypt passwords - defines the bcrypt complexity and time taken to be calculated
 const bcryptSalt = 10;
+//authentication middleware
 const passport = require("passport");
+//passport authentication strategy (this case with username and password)
 const LocalStrategy = require("passport-local").Strategy;
+//middleware which validates you are logged in - otherwise it redirects you
 const ensureLogin = require("connect-ensure-login");
+//holds temporary information which is self-destroyed after being used. One-off in the session
 const flash = require("connect-flash");
+//it is our favourite ODM - gives you functionality on top of mongodb
 const mongoose = require("mongoose")
+//this is needed in order to be able to pass information from html5 forms towards the views
 const bodyParser = require('body-parser');
+//path management
 const path = require('path')
+//handlebars templating 
 const hbs = require('hbs')
-
+//handlebars utilities
 Swag = require('swag');
-
+//here you boot up swag so it is available in the views (made with handlebars)
 Swag.registerHelpers(hbs);
 
 const User = require("./models/user");
@@ -70,9 +81,6 @@ passport.use(new LocalStrategy({
 	});
 }));
 
-app.use(passport.initialize());
-app.use(passport.session());
-
 passport.serializeUser((user, cb) => {
 	cb(null, user._id);
 });
@@ -85,6 +93,11 @@ passport.deserializeUser((id, cb) => {
 		cb(null, user);
 	});
 });
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 app.get('/', ensureLogin.ensureLoggedIn(), (req, res) => {
 	res.render('base', {
@@ -104,9 +117,9 @@ app.post("/signup", (req, res, next) => {
 	const password = req.body.password;
 
 	if (username === "" || password === "") {
-		res.render("auth/signup", {
+		res.render("base", {
 			message: "Indicate username and password",
-			"section": "private"
+			"section": "signup"
 		});
 		return;
 	}
